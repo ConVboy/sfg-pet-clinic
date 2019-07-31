ca32cac2c1064bcb0cb514fbf12c04340a956643
redie <e.redinger3@gmail.com>
2019-07-31 10:45:51 +0200

    Adding ID to model classes via BaseEntity class

--- pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package convboy.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
