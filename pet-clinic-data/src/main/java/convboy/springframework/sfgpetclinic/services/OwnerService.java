1e19c3eb9c995808c6306e18413f66c7fa53eaf9
redie <e.redinger3@gmail.com>
2019-07-31 11:00:37 +0200

    Refactor Service Interface to Common Base Interface

--- pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/services/OwnerService.java
@@ -4,13 +4,9 @@ import convboy.springframework.sfgpetclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
-
-  Owner findByID(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
 
   Owner findByLastName(String lastName);
 
-  Owner save(Owner owner);
 
-  Set<Owner> findAll();
 }
