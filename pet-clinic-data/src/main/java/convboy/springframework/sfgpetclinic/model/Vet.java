e1a3490b961f644c087d5db495d7b4e9ee274f76
redie <e.redinger3@gmail.com>
2019-10-10 13:51:27 +0200

    added specialty and added it to vet class

--- pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package convboy.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialites;
+
+    public Set<Specialty> getSpecialites() {
+        return specialites;
+    }
+
+    public void setSpecialites(Set<Specialty> specialites) {
+        this.specialites = specialites;
+    }
 }
