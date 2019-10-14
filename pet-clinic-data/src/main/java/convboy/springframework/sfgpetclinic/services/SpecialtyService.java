01affe79c16ff33ac5bd3a1357a35d70e5f33995
redie <e.redinger3@gmail.com>
2019-10-14 11:39:44 +0200

    Adding Speciality Mapping and load/save Data on startup

---
+++ pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package convboy.springframework.sfgpetclinic.services;
+
+import convboy.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
