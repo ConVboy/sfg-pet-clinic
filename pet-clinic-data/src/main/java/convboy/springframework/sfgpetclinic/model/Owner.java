a2037fcac611fe15a7efba55c5a43c1a7dab7784
redie <e.redinger3@gmail.com>
2019-10-15 16:30:23 +0200

    Updatet Owner and related classes with JPA Mappings

--- pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,21 @@
 package convboy.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "adress")
     private String adress;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public Set<Pet> getPets() {
