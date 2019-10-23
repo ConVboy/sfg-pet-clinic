d5c05be68a40a10bff8630e0c09432662770f219
redie <e.redinger3@gmail.com>
2019-10-23 17:07:18 +0200

    ownerservice reset to old state

--- pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/convboy/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,8 +1,10 @@
 package convboy.springframework.sfgpetclinic.services;
 
 import convboy.springframework.sfgpetclinic.model.Owner;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
